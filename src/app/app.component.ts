35978a617160c514e8afadd5fb4863725c88b817
Ganeshkanna0Varadhan <ganeshkanna718@gmail.com>
2025-02-21 14:22:10 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+  message: string = '';
+  isEnabled: boolean = false;
+  send() {
+    if (!this.message.trim()) {
+      alert("Suggestion value is not defined");
+      return;
+    }
+    this.isEnabled = true;
+    this.message = '';
+    setTimeout(() => {
+
+      this.isEnabled = false;
+      
+    }, 4000);
+  }
 }
